ef96f1f3aa94d0361eb9188cdb9c2c469817a08f
Leo Honkanen <leo.o.honkanen@gmail.com>
2016-02-26 19:43:42 +0200

    translate app names

--- tekis/board/apps.py
+++ tekis/board/apps.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+from django.utils.translation import ugettext as _
 
 
 class BoardConfig(AppConfig):
     name = 'board'
+    verbose_name = _('Board')
--- tekis/flatpages/apps.py
+++ tekis/flatpages/apps.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+from django.utils.translation import ugettext as _
 
 
 class FlatpagesConfig(AppConfig):
     name = 'flatpages'
+    verbose_name = _('Flatpages')
