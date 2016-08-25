82434288485ba7092b81ec6b3d7fcf25abbc35e6
Leo Honkanen <leo.o.honkanen@gmail.com>
2016-08-25 20:35:04 +0300

    lazy translations

--- tekis/flatpages/admin.py
+++ tekis/flatpages/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.conf import settings
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 from tekis.flatpages import models
 
